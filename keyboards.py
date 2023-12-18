d077bf6b65d069e0d34fff8407406c730458beba
Vladimir Privalov <vovaprivalov@gmail.com>
2023-12-18 11:53:02 +0500

    Handle click on disabled choice in inline keyboard

--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,9 @@
 from telegram import InlineKeyboardButton
 
 
-def day_time_choice_keyboard():
+def day_time_choice_keyboard(disabled_values=None):
+    print('day_time_choice_keyboard')
+    print(disabled_values)
     keyboard = [
         [
             InlineKeyboardButton("Утро", callback_data='empty'),
@@ -13,11 +15,14 @@ def day_time_choice_keyboard():
         keyboard_row = []
         for j in range(0, 3):
             hour = i + j * 5
-            button_text = f'{hour}:10'
-            callback_data = f'{hour}.10'
+            time_value = f'{hour}.10'
+            callback_data = 'empty' if time_value in disabled_values else time_value
+            button_text = '❌ ' if time_value in disabled_values else ''
+            button_text += f'{hour}:10'
             keyboard_row.append(InlineKeyboardButton(button_text, callback_data=callback_data))
         keyboard.append(keyboard_row)
     keyboard.append([
         InlineKeyboardButton("Подтвердить", callback_data='confirm'),
     ])
+
     return keyboard
