b6aaefa5812be600cf7dcbd7748e89b2892dbdc5
Vladimir Privalov <vovaprivalov@gmail.com>
2024-01-10 01:12:45 +0500

    Add method for recommendation details

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 import asyncio
 import aiohttp
+from requests_data import parse_view_date
 
+def get_recommendation_message(recommendation, allowed_dates_number):
+    booking_date = recommendation['data_list'].split('_')
+    booking_date_parse = parse_view_date(booking_date)
+    text = '''
+        *Подписчиков:* ''' + str(recommendation['subscribers']) + '''\n
+        *Охват:* ''' + str(recommendation['coverage']) + '''\n
+        *Стандартная цена:* ''' + str(recommendation['price_standart']) + '''\n
+        *Текущая цена:* ''' + str(recommendation['price_now']) + '''\n
+        *Формат:* ''' + recommendation['format'] + '''\n
+        *Собрано постов:* ''' + str(len(booking_date) - allowed_dates_number) + '''/''' + str(recommendation['number_posts']) + '''\n
+        *Места длля брони:* ''' + booking_date_parse + '''\n
+        *Ревизиты:* ''' + recommendation['requisites'] + '''\n
+        *Дедлайн формирования опта:* ''' + recommendation['deadline'] + '''\n
+        *Юзернейм:* [''' + recommendation['username'] + ''']\n
+        *Информация:* ''' + recommendation['info'] + '''\n
+        *Контакт для связи*: [@slon_feedback]'''
+    return text
 
 async def get_available_time_slots(id: int):
     recommendation_data = await get_recommendation_data(id)
