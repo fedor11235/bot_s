bee061fd3f1d980f86b5402e614358f7b61da5e7
Fedor <f.avdeev@anketolog.ru>
2024-01-10 17:12:06 +0700

    test print date

--- profile_opt.py
+++ profile_opt.py
@@ -277,16 +277,13 @@ async def profile_opt(update: Update, context) -> None:
         reply_markup = InlineKeyboardMarkup(keyboard)
         # opts = user_opt(user_id)
         for opt in opts:
+            print(opt)
             date_str = ''
             date_array = opt['booking_date'].split('_')
             time_array = opt['booking_time'].split('_')
-            print(opt['booking_date'])
-            print(date_array)
             for date in date_array:
-                try:
-                    date_str += ' ' + date.split('/')[1]
-                except:
-                    continue
+                if date != '':
+                  date_str += ' ' + date.split('/')[1]
 
             text += date_str + ' ' + opt['chanel'] + ' ' +' '.join(time_array) + '\n'
             # text += ' '.join(opt['booking_date'].split('_')) + ' ' + opt['chanel'] + ' ' + ' '.join(opt['placement_time'].split('_')) + '\n'
